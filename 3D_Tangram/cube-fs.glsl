50a7da2085c4c93f563df37193339787e8148a24
fallenatlas <tiago.a.p.antunes@gmail.com>
2023-12-10 20:59:15 +0000

    docs: cleanup comented code

--- 3D_Tangram/cube-fs.glsl
+++ 3D_Tangram/cube-fs.glsl
@@ -10,19 +10,8 @@ out vec4 FragmentColor;
 
 void main(void)
 {
-/** /
-  // vec3 color = vec3(1.0);
-  // vec3 color = (exPosition + vec3(1.0)) * 0.5;
-  // vec3 color = vec3(exTexcoord, 0.0);
-	vec3 color = (exNormal + vec3(1.0)) * 0.5;
-	FragmentColor = vec4(color,1.0);
-/**/
 	vec3 N = normalize(exNormal);
 	vec3 direction = normalize(vec3(1.0, 0.5, 0.25));
 	float intensity = max(dot(direction, N), 0.0) + 0.2f;
-	//FragmentColor = Color;
 	FragmentColor = Color * vec4(vec3(intensity), 1.0f);
-	//FragmentColor = vec4(vec3(intensity), 1.0);
-/**/
-	// cores tao estranhas pq isto ta em model space?
 }
